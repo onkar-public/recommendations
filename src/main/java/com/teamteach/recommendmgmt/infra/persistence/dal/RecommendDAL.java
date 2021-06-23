9b041e71eb04feb220bc1b48b44cd2dea5c56309
OnkarParmar <onkar.parmar@gmail.com>
2021-06-23 09:08:35 +0530

    monitor

--- src/main/java/com/teamteach/recommendmgmt/infra/persistence/dal/RecommendDAL.java
+++ src/main/java/com/teamteach/recommendmgmt/infra/persistence/dal/RecommendDAL.java
@@ -27,6 +27,7 @@ public class RecommendDAL  implements IRecommendRepository {
         TextCriteria criteria = TextCriteria.forDefaultLanguage().matchingAny(words);
 
         Query query = TextQuery.queryText(criteria);
+        System.out.println(query);
         return mongoTemplate.find(query, Recommendation.class);
     }
 
